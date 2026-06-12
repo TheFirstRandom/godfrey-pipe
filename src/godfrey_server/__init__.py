bed05a68c3637fdc5669ddcf0956ace2ef39ba81
TheFirstRandom <160148761+TheFirstRandom@users.noreply.github.com>
2026-06-12 14:15:58 +0200

    feat: supress warnings

--- src/godfrey_server/__init__.py
+++ src/godfrey_server/__init__.py
@@ -1,3 +1,8 @@
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning, message=".*dropout option.*")
+warnings.filterwarnings("ignore", category=FutureWarning, message=".*weight_norm.*")
+
 from . import server
 from . import models
 from .main import cli
